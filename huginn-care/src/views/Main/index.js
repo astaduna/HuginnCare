a9da13fb48397d11fdd8c8d0647fb362e2f568a4
anh0512 <thuvu0512@gmail.com>
2024-02-27 12:19:20 +0000

    change svg to png icons

--- huginn-care/src/views/Main/index.js
+++ huginn-care/src/views/Main/index.js
@@ -7,6 +7,7 @@ const Main = ({ navigation: { navigate } }) => (
     <View style={styles.container}>
         <Image source={dashboard} style={styles.logo} />
         <Text style={styles.paragraph}>The most powerful image manipulation application out there! Feel free to test out its powers!</Text>
+        <a href="https://www.flaticon.com/free-icons/dashboard" title="dashboard icons">Dashboard icons created by Pixel perfect - Flaticon</a>
         <TouchableHighlight
             style={styles.button}
             onPress={() => { navigate('Gallery') }}>
