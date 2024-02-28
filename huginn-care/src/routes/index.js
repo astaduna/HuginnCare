54f323d6af1044931c41fef661d5e3efaebb51d7
lorraineros <lorrainert2002@gmail.com>
2024-02-28 13:01:42 +0000

    add icons to navbar

--- huginn-care/src/routes/index.js
+++ huginn-care/src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-const Stack = createStackNavigator();
+
 const Tab = createBottomTabNavigator();
 
 import Main from '../views/Main';
@@ -10,13 +10,33 @@ import Reports from '../views/Reports';
 import Profile from '../views/Profile';
 
 const Routes = () => (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Main">
-        <Tab.Screen name="Main" component={Main} />
-        <Tab.Screen name="Reports" component={Reports} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  <NavigationContainer>
+    <Tab.Navigator
+      initialRouteName="Main"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconSource;
+
+          if (route.name === 'Main') {
+            iconSource = require('../resources/dashboard.png');
+          } else if (route.name === 'Reports') {
+            iconSource = require('../resources/file.png');
+          } else if (route.name === 'Profile') {
+            iconSource = require('../resources/user.png');
+          }
+          return <Image source={iconSource} style={{ width: 24, height: 24, tintColor: color }} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'black',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Main" component={Main} />
+      <Tab.Screen name="Reports" component={Reports} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
 
 export default Routes;
