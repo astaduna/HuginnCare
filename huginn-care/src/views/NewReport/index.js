a39ff7480f14f1eb8191b0bf5c70fc4efdafb175
astaduna <astaj22@ru.is>
2024-04-05 11:08:33 +0000

    Vann í NewReport & RadioButton

--- huginn-care/src/views/NewReport/index.js
+++ huginn-care/src/views/NewReport/index.js
@@ -1,43 +1,105 @@
-import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import RNPickerSelect from 'react-native-picker-select';
+import { View, Text, TextInput, ScrollView } from 'react-native';
+import RadioButton from '../../components/RadioButton';
 import styles from './styles';
 
-const NewReport = ({ navigation: { navigate } }) => (
+const NewReport = ({ navigation: { navigate } }) => {
+    const [checked, setChecked] = useState('yes'); //State for radio button
 
-    <View style={styles.container}>
+    return (
+    <ScrollView style={styles.container}>
         <View style={styles.formFrame}>
             <Text style={styles.title}>Almennar upplýsingar</Text>
-            <Text>Deild</Text>
+            <Text style={styles.inputTitle}>Deild</Text>
+            <RNPickerSelect
+            style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+            }}
+            placeholder={{ 
+                label: 'Veldu deild', 
+                value: null 
+            }}
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Deild 1', value: 'deild1' },
+                { label: 'Deild 2', value: 'deild2' },
+                { label: 'Deild 3', value: 'deild3' },
+            ]}
+            />
+            <Text style={styles.inputTitle}>þjónustuþegi</Text>
+            <RNPickerSelect
+            style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+            }}
+            placeholder={{ 
+                label: 'Veldu þjónustuþega', 
+                value: null 
+            }}
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Þjónustuþegi 1', value: 'þjónustuþegi1' },
+                { label: 'Þjónustuþegi 2', value: 'þjónustuþegi2' },
+                { label: 'Þjónustuþegi 3', value: 'þjónustuþegi3' },
+            ]}
+            />
+            <Text style={styles.inputTitle}>Tegund vaktar</Text>
+            <RNPickerSelect
+            style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+            }}
+            placeholder={{ 
+                label: 'Veldu tegund vaktar', 
+                value: null 
+            }}
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Vakt 1', value: 'vakt1' },
+                { label: 'Vakt 2', value: 'vakt2' },
+                { label: 'Vakt 3', value: 'vakt3' },
+            ]}
+            />
+            <Text style={styles.inputTitle}>Starfsmenn á vakt</Text>
             <TextInput
                 style={styles.input}
-                placeholder="Veldu deild"
-                keyboardType="default"
-            />
-            <Text>þjónustuþegi</Text>
-            <TextInput // Breyta í drop-down menu
-                style={styles.input}
-                placeholder="Veldu þjónustuþega"
-                keyboardType="default"
-            />
-            <Text>Tegund vaktar</Text>
-            <TextInput // Breyta í drop-down menu
-                style={styles.input}
-                placeholder="Veldu tegund vaktar"
-                keyboardType="default"
-            />
-            <Text>Starfsmenn á vakt</Text>
-            <TextInput
-                style={styles.input}
+                autoFocus
                 placeholder="Skrifaðu inn nöfn starfsmanna"
                 keyboardType="default"
             />
-            <Text>Voru lyf gefin?</Text>     
+            {/* <Text style={styles.inputTitle}>Voru lyf gefin?</Text>
+            <View style={styles.input}>
+                    <RadioButton
+                        value="yes"
+                        status={checked === 'yes' }
+                        onPress={() => setChecked('yes')}
+                    />
+                </View>
+            <View style={styles.input}>
+                    <RadioButton
+                        value="no"
+                        status={checked === 'no'}
+                        onPress={() => setChecked('no')}
+                    />
+                    <Text>Nei</Text>
+                </View>
+            <View style={styles.input}>
+                    <RadioButton
+                        value="notRelevant"
+                        status={checked === 'notReleveant'}
+                        onPress={() => setChecked('notRelevant')}
+                    />
+                    <Text>Á ekki við</Text>
+                </View>     */}
         </View>
 
         <View style={styles.formFrame}>
             <Text style={styles.title}>Dagssamningar</Text>
         </View>
-    </View>
-);
+    </ScrollView>
+    );
+        };
 
 export default NewReport;
--- huginn-care/src/views/NewReport/styles.js
+++ huginn-care/src/views/NewReport/styles.js
@@ -4,28 +4,48 @@ import { blueIcon, greenIcon } from '../../styles/colors';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        padding: 15,
-        justifyContent: 'start',
+        padding: 15
     },
     formFrame: {
-        borderBlockColor: 'lightgrey',
-        borderWidth: 2,
-        borderRadius: 5,
+        marginTop: 10,
+        marginBottom: 5,
         padding: 20,
-        width: '95%',
-        margin: 10,
+        borderColor: 'gainsboro',
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: 'white'
     },
     title: {
-        margin: 10,
-        alignContent: 'center',
-        textTransform: 'uppercase',
+        marginTop: 15,
+        fontSize: 20,
         fontWeight: 'bold',
+        color: '#797979'
+    },
+    inputTitle: {
+        marginTop: 15,
+        marginLeft: 10,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#797979'
     },
     input: {
-        margin: 10,
-        padding: 10,
-        borderWidth: 1,
-        borderColor: 'lightgrey',
-        borderRadius: 2,
+        padding: 15,
+        margin: 7,
+        borderRadius: 5,
+        borderColor: 'gainsboro',
+        borderWidth: 2,
+        fontSize: 13
+    },
+    radioButtons: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginBottom: 10,
+        justifyContent: 'space-around'
+    },
+    radioButtonInner: {
+      width: 12,
+      height: 12,
+      borderRadius: 6,
+      backgroundColor: '#007AFF',
     },
 });
